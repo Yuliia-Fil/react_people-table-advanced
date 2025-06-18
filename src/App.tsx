c3be80db08abc96a0dab502e09c0ee26e6e69a9c
Yulia Fil <filyulia.yf@gmail.com>
2025-06-18 10:36:04 +0300

    add filtration logic from search params

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
 import { PeoplePage } from './components/PeoplePage';
-import { Navbar } from './components/Navbar';
 
 import './App.scss';
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
+import { HomePage } from './components/HomePage';
+import { NotFoundPage } from './components/NotFoundPage';
+import { NavBar } from './components/NavBar';
 
 export const App = () => {
+  const location = useLocation();
+
+  if (location.pathname === '/home') {
+    return <Navigate to="/" replace={true} />;
+  }
+
   return (
     <div data-cy="app">
-      <Navbar />
-
-      <div className="section">
+      <NavBar />
+      <main className="section">
         <div className="container">
-          <h1 className="title">Home Page</h1>
-          <h1 className="title">Page not found</h1>
-          <PeoplePage />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="people" element={<PeoplePage />}>
+              <Route index element={<PeoplePage />} />
+              <Route path=":slug" element={<PeoplePage />} />
+            </Route>
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
         </div>
-      </div>
+      </main>
     </div>
   );
 };
--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -1,8 +1,75 @@
 import { PeopleFilters } from './PeopleFilters';
 import { Loader } from './Loader';
 import { PeopleTable } from './PeopleTable';
+import { useEffect, useMemo, useState } from 'react';
+import { Person } from '../types';
+import { getPeople } from '../api';
+import { useLocation } from 'react-router-dom';
+
+const getPreparedPeople = (
+  people: Person[],
+  sexFilter: string | null,
+  queryFilter: string | null,
+  centuriesFilter: string[],
+) => {
+  let preparedPeople = [...people];
+
+  if (sexFilter) {
+    preparedPeople = preparedPeople.filter(person => person.sex === sexFilter);
+  }
+
+  if (queryFilter) {
+    preparedPeople = preparedPeople.filter(person => {
+      const name = person.name.toLowerCase();
+      const motherName = person.motherName
+        ? person.motherName.toLowerCase()
+        : '';
+      const fatherName = person.fatherName
+        ? person.fatherName.toLowerCase()
+        : '';
+      const qwery = queryFilter.toLowerCase();
+
+      return (
+        name.includes(qwery) ||
+        motherName.includes(qwery) ||
+        fatherName.includes(qwery)
+      );
+    });
+  }
+
+  if (centuriesFilter.length > 0) {
+    preparedPeople = preparedPeople.filter(person => {
+      const bornCentury = Math.ceil(person.born / 100);
+
+      return centuriesFilter.some(century => +century === bornCentury);
+    });
+  }
+
+  return preparedPeople;
+};
 
 export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const sexFilter = searchParams.get('sex');
+  const queryFilter = searchParams.get('query');
+  const centuriesFilter = searchParams.getAll('centuries');
+
+  const preparedPeople = useMemo(
+    () => getPreparedPeople(people, sexFilter, queryFilter, centuriesFilter),
+    [people, sexFilter, queryFilter, centuriesFilter],
+  );
+
+  useEffect(() => {
+    getPeople()
+      .then(peopleFromServer => setPeople(peopleFromServer))
+      .catch(() => setError(true))
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <>
       <h1 className="title">People Page</h1>
@@ -10,20 +77,35 @@ export const PeoplePage = () => {
       <div className="block">
         <div className="columns is-desktop is-flex-direction-row-reverse">
           <div className="column is-7-tablet is-narrow-desktop">
-            <PeopleFilters />
+            {people.length > 0 && !loading && !error && (
+              <div className="block">
+                <div className="box table-container">
+                  <PeopleFilters />
+                </div>
+              </div>
+            )}
           </div>
 
           <div className="column">
             <div className="box table-container">
-              <Loader />
-
-              <p data-cy="peopleLoadingError">Something went wrong</p>
-
-              <p data-cy="noPeopleMessage">There are no people on the server</p>
+              {loading && <Loader />}
+              {error && (
+                <p data-cy="peopleLoadingError">Something went wrong</p>
+              )}
+              {people.length === 0 && !loading && !error && (
+                <p data-cy="noPeopleMessage">
+                  There are no people on the server
+                </p>
+              )}
 
               <p>There are no people matching the current search criteria</p>
-
-              <PeopleTable />
+              {people.length > 0 && !loading && !error && (
+                <div className="block">
+                  <div className="box table-container">
+                    <PeopleTable people={preparedPeople} />
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
